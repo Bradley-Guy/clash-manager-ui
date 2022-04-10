2ac07cafe04ee82c236d4b0244fedd7cc9a189f3
Nurivan Gomez <nurivan.gomez@gmail.com>
2022-04-10 13:39:39 -0500

    Table is a separate component

--- src/App.js
+++ src/App.js
@@ -1,96 +1,26 @@
 import './App.css';
 import axios from 'axios';
 import React from 'react';
+import Table from './components/Table.js'
 
 function App() {
   const url = "http://localhost:7071/api/ClanStats?clanTag=9YQQQ98";
-  const [post, setPost] = React.useState(null);
+  const [data, setData] = React.useState(null);
 
   React.useEffect(() => {
     axios.get(url).then((response) => {
-      console.log("URL got");
-      setPost(response.data);
+      setData(response.data);
     });
   }, []);
 
-  console.log("i stay noided");
+  if (!data) return <Table/>;
 
-  if (!post) return null;
-
-  return(
+  return (
     <div className="App">
-    <table>
-      <thead>
-        <tr>
-          <th>Username</th>
-          <th>Score</th>
-        </tr>
-      </thead>
-      <tbody>
-        {post.ranks.map(item => {
-          return(
-            <tr key={item[1].name}>
-              <td>{item[1].name}</td>
-              <td> { item[1].score}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+      <Table data={data}/>;
     </div>
   );
 }
 
 export default App;
 
-/*import logo from './logo.svg';
-import axios from 'axios';
-import React from 'react';
-import './App.css';
-
-function App() {
-  const url = "http://localhost:7071/api/ClanStats?clanTag=9YQQQ98";
-  const [post, setPost] = React.useState(null);
-
-  React.useEffect(() => {
-    axios.get(url).then((response) => {
-      console.log("URL got");
-      setPost(response.data);
-    });
-  }, []);
-
-  if (!post) return null;
-
-  return (
-    <div>
-      <p>Results:</p>
-      <h1>{post.ranks[0][0]}</h1>
-
-    </div>
-  );
-
-  /*return(
-    <div className="App">
-    <table>
-      <thead>
-        <tr>
-          <th>Username</th>
-          <th>Score</th>
-        </tr>
-      </thead>
-      <tbody>
-        {post.ranks.map(item => {
-          return(
-            <tr key={item[1].name}>
-              <td>{item[1].name}</td>
-              <td> { item[1].score}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
-    </div>
-  );
-}
-
-export default App;*/
---
+++ src/components/Table.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+class Table extends React.Component {
+    render() {
+        if(this.props.data) {
+            return (
+            <table>
+                <thead>
+                <tr>
+                    <th>Rank</th>
+                    <th>Username</th>
+                    <th>Score</th>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                this.props.data.ranks.map((item, index) => {
+                    return(
+                    <tr key={item[1].name}>
+                        <td>{index + 1}</td>
+                        <td>{item[1].name}</td>
+                        <td> { item[1].score}</td>
+                    </tr>
+                    );
+                })}
+                </tbody>
+            </table>
+            );
+        } else {
+            return (
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Rank</th>
+                        <th>Username</th>
+                        <th>Score</th>
+                    </tr>
+                    </thead>
+                </table>
+            );
+            
+        }
+    }
+}
+export default Table;
