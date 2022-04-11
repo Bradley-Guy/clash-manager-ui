48ce39b3edec436d0e4cd98d3a355f6aef20cfe3
Bradley Guy <1.chartcove@gmail.com>
2022-04-10 20:39:06 -0500

    Added topcard

---
+++ src/components/Card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Card extends React.Component {
+    render() {
+        if(this.props.data) {
+            let url = this.props.data.favouriteCard.imageUri;
+            let cardName = this.props.data.favouriteCard.name;
+            return (
+            <div>
+                <p>Favourite Card:</p>
+                <img src={url}/>
+                <img alt={""}/>
+                <p>{cardName}</p>
+            </div>
+            )
+        }
+    }
+
+}
+export default Card;
