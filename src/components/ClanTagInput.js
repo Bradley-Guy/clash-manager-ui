b2bfe096f7b480b2649a75a251c50dafcc929cc7
Nurivan Gomez <nurivan.gomez@gmail.com>
2022-04-11 23:05:07 -0500

    Added useless sliders

--- src/components/ClanTagInput.js
+++ src/components/ClanTagInput.js
@@ -54,7 +54,11 @@ class ClanTagInput extends React.Component {
                 <button onClick={(e) => this.toggleDropdown()}><span className='material-icons'>tune</span></button>
                 </div>
                 <div className={this.state.showTuning}>
-                    <Slider name="test"/>
+                    <Slider name="Trophies"/>
+                    <Slider name="Last Online"/>
+                    <Slider name="Cards Donated"/>
+                    <Slider name="Cards Received"/>
+                    <Slider name="War Score"/>
                 </div>
             </div>
          );
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -2,8 +2,19 @@ import React from 'react';
 import classes from './Slider.module.css'
 
 class Slider extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: 1
+        }
+    }
     render() {
-        return (<div className={classes.slider}><p>{this.props.name}</p><input type="range" min="0" max="1" defaultValue="0.5" step="0.1"></input></div>);
+        return (
+        <div className={classes.slider}>
+            <p>{`${this.props.name}: ${(Math.round(this.state.value * 100) / 100).toFixed(2)}`}</p>
+            <input type="range" min="0" max="1" defaultValue="1" step="0.01" onChange={e => this.setState({value: e.target.value})}></input>
+        </div>
+        );
     }
 
 }
